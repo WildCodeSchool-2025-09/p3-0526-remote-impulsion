546b05d0c2654eba035b51ac1b293b2d5ad3f668
Cynthiou <cynthia.flores@hotmail.fr>
2026-09-08 23:03:03 +0200

    feat(US11): add prepared session screen

--- client/src/pages/Session.tsx
+++ client/src/pages/Session.tsx
@@ -1,9 +1,18 @@
+import usePreparedSessions from "../hooks/usePreparedSessions";
+
 function Session() {
   return (
-    <h1 className="text-2xl font-display italic font-extrabold uppercase">
-      Séance
-    </h1>
+    <>
+      <h1 className="text-2xl font-display italic font-extrabold uppercase">
+        Séance
+      </h1>
+
+      <button type="button">Ajouter des exercices</button>
+
+      <button type="button" disabled>
+        Démarrer
+      </button>
+    </>
   );
 }
-
 export default Session;
