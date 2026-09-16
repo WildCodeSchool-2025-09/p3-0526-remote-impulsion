e818f385a6c5265ad55715a9d2afbba395a02548
Ranskalainen-wq <fabian.janczewski@gmail.com>
2026-09-16 20:28:39 +0200

    fix(US03): correct authTypes import casing

--- server/src/modules/auth/authActions.ts
+++ server/src/modules/auth/authActions.ts
@@ -1,7 +1,7 @@
 import argon2 from "argon2";
 import type { RequestHandler } from "express";
-import type { RegisterPayload } from "./AuthTypes";
 import authRepository from "./authRepository";
+import type { RegisterPayload } from "./authTypes";
 
 const register: RequestHandler = async (req, res, next) => {
   try {
